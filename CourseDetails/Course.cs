24b5c37233fabb6ff687affb9c3c41a11f276531
Pravin2818 <pravinmore1118@gmail.com>
2024-12-07 10:25:00 +0530

    commit1

---
+++ CourseDetails/Course.cs
@@ -0,0 +1,72 @@
+﻿namespace CourseDetails //DO NOT change the namespace name
+{
+    public class Course //DO NOT change the class name
+    {
+        //Implement your code here
+        //Program program = new Program();
+        public void AddCourseDetails(string name, int fee)
+        {
+            if (!Program.CourseDetails.ContainsKey(name))
+            {
+
+                Program.CourseDetails.Add(name, fee);
+                Console.WriteLine("Course details added successfully");
+
+            }
+            else
+            {
+                Console.WriteLine("Course already exist");
+            }
+
+            //Program.CourseDetails[name] = fee;
+
+        }
+
+        public void RemoveCourseDetails(string name) {
+
+            if (Program.CourseDetails.ContainsKey(name))
+            {
+
+                Program.CourseDetails.Remove(name);
+                Console.WriteLine("Course details removed sucessfully");
+
+            }
+            else
+            {
+                Console.WriteLine("course not found");
+            }
+            //Program.CourseDetails.Remove(name);
+        }
+
+        public Dictionary<string, int> SortCourseByFee() {
+
+
+            var sortedCourses = Program.CourseDetails
+                                        .OrderBy(course => course.Value)
+                                        .ToDictionary(course => course.Key, course => course.Value);
+
+            // Display sorted courses
+            Console.WriteLine("Course Name\tFee");
+            foreach (var course in sortedCourses)
+            {
+                Console.WriteLine($"{course.Key}\t{course.Value}");
+            }
+
+            return sortedCourses;
+
+
+            //var sortedCourses = Program.CourseDetails.OrderBy(x => x.Value);
+
+            //    //.ToDictionary(x => x.Key, x => x.Value);
+
+            //Dictionary<string, int> result = new Dictionary<string, int>();
+            //Console.WriteLine("course name\t fee");
+            //foreach (var c in sortedCourses) {
+
+            //    result.Add(c.Key, c.Value);
+            //}
+            //return result;
+        }
+
+    }
+}
---
+++ CourseDetails/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace CourseDetails //DO NOT change the namespace name
+{
+    public class Program //DO NOT change the class name    
+    {
+        //Implement the property here
+
+        public static Dictionary<string, int> CourseDetails { get; set; } = new Dictionary<string, int>();
+
+        public static void Main(string[] args)  //DO NOT change the method signature
+        {
+            //Implement your code here
+
+            Course course = new Course();
+
+            Console.WriteLine("1.Add Course Details");
+            Console.WriteLine("2.Remove Course Details");
+            Console.WriteLine("3.Sort Course By Fee");
+            Console.WriteLine("4.Exit");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the course name");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("Enter the course fee");
+            int courseFee = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                course.AddCourseDetails(courseName, courseFee);
+                Console.WriteLine("Course details added successfully");
+            }
+            else if (choice == 2)
+            {
+
+                course.RemoveCourseDetails(courseName);
+                Console.WriteLine("Course details removed sucessfully");
+            }
+            else if (choice == 3)
+            {
+
+                course.SortCourseByFee();
+            }
+            else if (choice == 4)
+            {
+
+                Console.WriteLine("Thank you \n exit");
+            }
+            else {
+
+                Console.WriteLine("Invalid choice. Please enter a valid option.");
+            }
+        }
+    }
+}
