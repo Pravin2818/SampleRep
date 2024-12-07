8a6be79df022b763c0bfe1bed8da53e5866f2915
Pravin2818 <pravinmore1118@gmail.com>
2024-12-07 10:29:37 +0530

    commit2

---
+++ Bank Transactions App 161124123438/BankAccount/Account.cs
@@ -0,0 +1,70 @@
+﻿namespace BankAccount //DO NOT change the namespace name
+{
+    public class Account //DO NOT change the class name
+    {
+        //Implement your code here
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public decimal Deposit(decimal amount)
+        {
+            try
+            {
+                if (amount <= 0)
+                {
+
+                    throw new ArgumentException("Deposit amount must be positive.");
+
+                }
+
+                Balance += amount;
+                return Balance;
+            }
+
+            catch (ArgumentException ex) { 
+            
+                Console.WriteLine(ex.Message);
+                return Balance;
+            }
+
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            try
+            {
+                if (amount <= 0)
+                {
+
+                    throw new ArgumentException("Withdrawal amount must be positive.");
+                }
+
+                if (amount > Balance)
+                {
+                    throw new InvalidOperationException("Insufficient funds");
+
+
+                }
+
+                Balance -= amount;
+                return Balance;
+            }
+
+
+
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return Balance;
+            }
+
+            catch (InvalidOperationException ex) { 
+            
+                Console.WriteLine(ex.Message);
+                return Balance;
+            }
+
+        }
+    }
+}
---
+++ Bank Transactions App 161124123438/BankAccount/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Transactions;
+
+namespace BankAccount //DO NOT change the namespace name
+{
+    public class Program //DO NOT change the class name
+    {
+        public static void Main(string[] args)  //DO NOT change the method signature
+        {
+            //Implement your code here
+
+            Console.WriteLine("1.Deposit");
+            Console.WriteLine("2.Withdraw");
+            Console.WriteLine("Enter the choice");    
+            
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Account Number");
+            string accountNumber = Console.ReadLine();
+            Console.WriteLine("Enter the balance");
+            decimal balance = Convert.ToDecimal(Console.ReadLine());
+            
+
+
+            Account account = new Account()
+            {
+
+
+                AccountNumber = accountNumber,
+                Balance = balance,
+                //amount1 = amount
+
+            };
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter the amount to be deposit");
+                decimal amount1 = Convert.ToDecimal(Console.ReadLine());
+                decimal balanceAmount = account.Deposit(amount1);
+                Console.WriteLine($"Balance amount: {balanceAmount}");
+
+
+            }
+            else if (choice == 2)
+            {
+
+                Console.WriteLine("Enter the amount to be withdraw");
+                decimal amount2 = Convert.ToDecimal(Console.ReadLine());
+                decimal balanceAmount = account.Withdraw(amount2);
+                Console.WriteLine($"Balance amount: {balanceAmount}");
+            }
+
+            else {
+
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
